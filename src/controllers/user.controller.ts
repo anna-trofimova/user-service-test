f35d4f79510c2263891ecd47df91ef3f3d934fa2
anna-trofimova <cobrana1000@gmail.com>
2026-03-10 17:10:42 +0100

    add JWT login and auth middleware

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import * as userService from "../services/user.service";
+import { generateToken } from "../utils/jwt";
+import bcrypt from "bcrypt";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -8,5 +10,26 @@ export const register = async (req: Request, res: Response) => {
     res.json(user);
   } catch (error) {
     res.status(400).json({ message: "User creation failed" });
+  }
+};
+
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userService.findUserByEmail(email);
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) return res.status(400).json({ message: "Invalid password" });
+
+    const token = generateToken(user.id, user.role);
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Login error" });
   }
 };
